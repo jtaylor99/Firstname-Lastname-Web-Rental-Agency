16ca8c6a043e2154faa97513dc5d657b4829bdb2
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-10-15 15:19:17 -0500

    I made some data for handlebars

---
+++ data.js
@@ -0,0 +1,52 @@
+const Page_Data = {
+    movies: {
+        movie1: {
+            title: "Black Panther",
+            image:
+                "https://is5-ssl.mzstatic.com/image/thumb/Video125/v4/85/b8/cf/85b8cf19-30f8-4b2d-c4e4-87eb52e2ba28/pr_source.lsr/268x0w.png",
+            description:
+                "After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king.",
+            stock: 2
+        },
+        movie2: {
+            title: "Avengers Infinity War",
+            image:
+                "http://www.gstatic.com/tv/thumb/v22vodart/12863030/p12863030_v_v8_ae.jpg",
+            description:
+                "The Avengers unite to battle their most powerful enemy yet Thanos. On a mission to collect all six Infinity Stones.",
+            stock: 2
+        },
+        movie3: {
+            title: "Deadpool 2",
+            image:
+                "https://media.services.cinergy.ch/media/box1600/ca93e72443de5c0a69c1c45fafabef2a56d57aca.jpg",
+            description:
+                "Deadpool meets Russell, an angry teenager who lives at an orphanage.Deadpool realizes that he has to save him.",
+            stock: 3
+        },
+        movie4: {
+            title: "Star Wars: The Empire strikes back",
+            image:
+                "http://www.gstatic.com/tv/thumb/v22vodart/8884/p8884_v_v8_ag.jpg",
+            description:
+                " Luke Skywalker , Han Solo, Princess Leia and Chewbacca face attack by the Imperial forces on the ice planet Hoth.",
+            stock: 4
+        },
+        movie5: {
+            title: "The Negotiator",
+            image:
+                "http://www.gstatic.com/tv/thumb/v22vodart/21443/p21443_v_v8_ad.jpg",
+            description:
+                "Danny Roman (Samuel L. Jackson) is considered the best police hostage negotiator in Chicago. After a friend warns him thatsomeone is embezzling from a disability fund, the person is found dead.",
+            stock: 4
+        },
+        movie6: {
+            title: "Goodfellas",
+            image:
+                "http://www.gstatic.com/tv/thumb/v22vodart/12720/p12720_v_v8_an.jpg",
+            description:
+                "A young man grows up in the mob and works very hard to advance himself through the ranks. He enjoys his life of money and luxury, but is oblivious to the horror that he causes.",
+            stock: 5
+        }
+    }
+};
