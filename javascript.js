d203406e45953d5f43c39589b871106e57785586
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-10-17 12:46:26 -0500

    I added some javascript

--- javascript.js
+++ javascript.js
@@ -1,13 +1,47 @@
-function rent() {
-    var rentButton = document.getElementById("Rent");
-    rentButton.addEventListener("onclick", function(event) {
-        var x = document.getElementById("stock");
-        if (x > 0) {
-            x -= 1;
+function alertthankYou() {
+    alert("Thank You!!!!!!!!!!!");
+}
+
+function thankYou() {
+    var x = document.getElementById("save");
+    x.setAttribute("onclick", "alertthankYou()");
+}
+function nameContains() {
+    const rentInformationForm = document.forms["rent-information-form"];
+    const nameInput = rentInformationForm.elements["name"];
+    nameInput.addEventListener("blur", function(event) {
+        const letters = /^[A-Za-z]+$/;
+        const name = nameInput.value;
+        if (name.match(letters) && name.length > 3 && name.length < 20) {
+            nameInput.classList.remove("bg-danger");
+            nameInput.classList.add("bg-success");
         } else {
-            rentButton.setAttribute("disabled", true);
+            nameInput.classList.remove("bg-success");
+            nameInput.classList.add("bg-danger");
         }
     });
 }
-
-rent();
+function emailContains() {
+    const rentInformationForm = document.forms["rent-information-form"];
+    const emailInput = rentInformationForm.elements["email"];
+    emailInput.addEventListener("blur", function(event) {
+        const letters = /^[A-Za-z]+$/;
+        const numbers = /^[0-9]+$/;
+        const specialCh = /^[@.]+$/;
+        const email = emailInput.value;
+        if (
+            email.match(letters) ||
+            email.match(numbers) ||
+            email.match(specialCh)
+        ) {
+            emailInput.classList.remove("bg-danger");
+            emailInput.classList.add("bg-success");
+        } else {
+            emailInput.classList.remove("bg-success");
+            emailI.classList.add("bg-danger");
+        }
+    });
+}
+thankYou();
+nameContains();
+emailContains();
